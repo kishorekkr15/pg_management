bfeea08667a2581cf31c8ba08311b2bf35aa4bad
kishorekumar <kishorekumar.s@quantzi.in>
2025-10-25 16:58:59 +0530

    first commit

---
+++ src/main/java/com/example/pg_manager/PgManagerApplication.java
@@ -0,0 +1,13 @@
+package com.example.pg_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PgManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PgManagerApplication.class, args);
+	}
+
+}
